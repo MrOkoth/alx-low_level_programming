bd53fdcefb44504cac25c81d5184ebf416cca996
MrOkoth <Teddyokoth52@gmail.com>
2023-09-13 00:37:34 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer -prints minutes
+ *
+ * Return: Time
+ */
+
+void jack_bauer(void)
+{
+	int t, e;
+
+	for (t = 0 ; t < 24 ; t++)
+	{
+		for (e = 0 ; e < 60 ; e++)
+		{
+			_putchar(t / 10 + '0');
+			_putchar(t % 10 + '0');
+			_putchar(':');
+			_putchar(e / 10 + '0');
+			_putchar(e % 10 + '0');
+		}
+	}
+}

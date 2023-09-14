e5cb738951a9f5d8a9572833c8ac79d34aa6b9c3
MrOkoth <Teddyokoth52@gmail.com>
2023-09-14 06:42:08 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * print_numbers - Print numbers
+ *
+ * Return: 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar('\n');
+	_putchar('\n');
+}

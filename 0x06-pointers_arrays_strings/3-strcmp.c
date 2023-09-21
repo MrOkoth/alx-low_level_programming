fc476aaa62d5ac3af4924f7089ec620fc5ab3015
MrOkoth <Teddyokoth52@gmail.com>
2023-09-21 02:37:51 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,4 +7,19 @@
  * Return: int
  */
 
-int _str
+int _strcmp(char *s1, char *s2)
+{
+	int t;
+
+	for (t = 0 ; s1[t] != '\0' || s2[t] != '\0' ; t++)
+	{
+		if (s1[t] != s2[t])
+		{
+			if (s1[t] < s2[t])
+				return (s1[t] - s2[t]);
+			else if (s2[t] < s1[t])
+				return (s1[t] - s2[t]);
+		}
+	}
+	return (0);
+}

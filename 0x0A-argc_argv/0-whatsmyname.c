70a41c2711735547bc761366ee37d7007b0d2e1c
MrOkoth <Teddyokoth52@gmail.com>
2023-09-28 23:55:49 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: p
+ * @argv: p
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}

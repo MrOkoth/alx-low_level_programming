7dae4420d5d1ccc9b64f3246a7fb37eac22b3da6
MrOkoth <Teddyokoth52@gmail.com>
2023-09-29 00:04:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print number of argument
+ * @argc: p
+ * @argv: p
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}

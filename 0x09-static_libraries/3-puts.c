9f911b17f78af232390c6de071cb2fcf305da50d
MrOkoth <Teddyokoth52@gmail.com>
2023-09-30 01:49:28 -0700

    it s

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,15 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
+
 /**
  * main - Entry point
  * _puts - Description
  * Return: Always 0 (Success)
+ *
  */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
